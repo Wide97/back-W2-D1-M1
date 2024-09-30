1fcbc5b0ac454b4f1183afaf46c9efaa5efdd75c
Wide97 <mwidesott@gmail.com>
2024-09-30 15:48:20 +0200

    es 1

--- src/ESERCIZIO1/Main1.java
+++ src/ESERCIZIO1/Main1.java
@@ -1,5 +1,7 @@
 package ESERCIZIO1;
 
+import java.util.Scanner;
+
 public class Main1 {
     public static void main(String[] args) {
 
@@ -11,6 +13,23 @@ public class Main1 {
         for (int numero : numeriCasuali) {
             System.out.println(numero);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci la posizione del numero che vuoi aggiornare: ");
+        int posizione = scanner.nextInt();
+
+        System.out.println("Inserisci il numero che vuoi aggiornare : ");
+        int nuovoNumero = scanner.nextInt();
+
+        numeriCasuali[posizione] = nuovoNumero;
+
+        System.out.println("nuovo stato dell' Array: ");
+        for (int numero : numeriCasuali) {
+            System.out.println(numero);
+        }
+
+        scanner.close();
+
 
     }
 }
