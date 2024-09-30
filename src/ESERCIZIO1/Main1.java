214552dd90e4e8f837684df4e907de25b01489a9
Wide97 <mwidesott@gmail.com>
2024-09-30 15:56:54 +0200

    finish ex 1

--- src/ESERCIZIO1/Main1.java
+++ src/ESERCIZIO1/Main1.java
@@ -15,21 +15,36 @@ public class Main1 {
         }
 
         Scanner scanner = new Scanner(System.in);
+
+        try {
+
+
         System.out.println("Inserisci la posizione del numero che vuoi aggiornare: ");
         int posizione = scanner.nextInt();
+
+        if (posizione == 0){
+            System.out.println("Programma terminato");
+            scanner.close();
+            return;
+        }
 
         System.out.println("Inserisci il numero che vuoi aggiornare : ");
         int nuovoNumero = scanner.nextInt();
 
-        numeriCasuali[posizione] = nuovoNumero;
+        numeriCasuali[posizione - 1] = nuovoNumero;
 
         System.out.println("nuovo stato dell' Array: ");
         for (int numero : numeriCasuali) {
             System.out.println(numero);
         }
-
-        scanner.close();
-
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("errore: la posizione inserita è fuori limite");
+        } catch (Exception e) {
+            System.out.println("Errore: si è verificato un problema ");
+        } finally {
+            scanner.close();
+            System.out.println("Scanner chiuso");
+        }
 
     }
 }
